ace54dcc33cc3803ca5eb81d7837dfb53a8a45cb
“Craig <97258442+craigbucher@users.noreply.github.com>
2022-06-09 14:08:40 -0500

    initial js

--- js/binarySearch.js
+++ js/binarySearch.js
@@ -1,9 +1,31 @@
-function binarySearch(){
-  // Code goes here!
+function binarySearch(inputArray, target) {
+    // create a duplicate of input to work with
+    let workingArray = inputArray;
+    // iterate over the input array
+    for (let i = 0; i < inputArray.length; i++) {
+        // calculate midpoint of array
+        let midpointIndex = Math.floor(inputArray.length / 2);
+        let midpoint = workingArray[midpointIndex];
+        // compare value of midpoint to value of target
+        if (midpoint > target) {
+            // if greater than target, remove second half of array and re-iterate
+            workingArray = workingArray.slice(0, midpointIndex);
+        } else if (midpoint <
+            target) {
+            // if less than target, remove first half of array ad re-iterate
+            workingArray = workingArray.slice(midpointIndex, -1);
+        } else if (midpoint = target) {
+            // if equal to the target, return the index value
+            return 'target found at index ' + inputArray.indexOf(midpoint);
+        }
+    }
+    // if value not found, return null
+    return null;
+}
 }
 
-var smallArray = [1,2,3,4,5]
-var largeArray = [1,5,7,2,3,8,4,9]
+let smallArray = [1, 2, 3, 4, 5]
+let largeArray = [1, 5, 7, 2, 3, 8, 4, 9]
 
 console.log(binarySearch(1, smallArray) === 0);
 console.log(binarySearch(2, smallArray) === 1);
