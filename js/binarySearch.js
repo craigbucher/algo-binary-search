45da508dab2931a093f4a3017b3a71a251b7d220
“Craig <97258442+craigbucher@users.noreply.github.com>
2022-06-09 14:31:41 -0500

    working js

--- js/binarySearch.js
+++ js/binarySearch.js
@@ -1,31 +1,34 @@
-function binarySearch(inputArray, target) {
+function binarySearch(target, inputArray) {
     // create a duplicate of input to work with
     let workingArray = inputArray;
     // iterate over the input array
     for (let i = 0; i < inputArray.length; i++) {
+        //console.log('working array is ' + workingArray)
         // calculate midpoint of array
         let midpointIndex = Math.floor(inputArray.length / 2);
         let midpoint = workingArray[midpointIndex];
+        //console.log('midpoint is ' + midpoint + ' target is ' + target)
         // compare value of midpoint to value of target
         if (midpoint > target) {
             // if greater than target, remove second half of array and re-iterate
-            workingArray = workingArray.slice(0, midpointIndex);
-        } else if (midpoint <
-            target) {
+            workingArray = workingArray.slice(midpointIndex);
+        } else if (midpoint < target) {
             // if less than target, remove first half of array ad re-iterate
-            workingArray = workingArray.slice(midpointIndex, -1);
+            workingArray = workingArray.slice(0, midpointIndex);
         } else if (midpoint = target) {
             // if equal to the target, return the index value
-            return 'target found at index ' + inputArray.indexOf(midpoint);
+            //return('target found at index ' + inputArray.indexOf(midpoint));
+            return inputArray.indexOf(midpoint);
         }
     }
     // if value not found, return null
     return null;
 }
-}
 
 let smallArray = [1, 2, 3, 4, 5]
 let largeArray = [1, 5, 7, 2, 3, 8, 4, 9]
+
+// console.log(binarySearch(7, largeArray));
 
 console.log(binarySearch(1, smallArray) === 0);
 console.log(binarySearch(2, smallArray) === 1);
